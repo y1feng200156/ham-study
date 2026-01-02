624afd56c43d7ac3404ad8da217db118a9174fc6
Charles <hi@charlesify.com>
2026-01-03 00:16:54 +0800

    feat: Add viewport meta tag for responsive design.

--- app/root.tsx
+++ app/root.tsx
@@ -41,6 +41,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN">
       <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
